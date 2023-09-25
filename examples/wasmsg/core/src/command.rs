dabd47fb363a548787e63e3aa545531c5cd4ed7c
ademcaglin <adem.caglin@gmail.com>
2023-09-25 16:35:42 +0300

    Command

---
+++ examples/wasmsg/core/src/command.rs
@@ -0,0 +1,51 @@
+pub struct PureCommand {
+    pub channel: DigestId,                 // Channel identifier
+    pub context: DigestId,                 // Wasm identifier
+    pub query: BTreeMap<String, DigestId>, // Key value id e.g. xxx -> id
+    pub payload: Vec<u8>,                  // Encoded message payload
+    pub event: DigestId,                   // Result identifier
+}
+
+pub struct PureMessage {
+    pub command: PureCommand, // Command payload
+    pub previous: DigestId,   // Previous message id
+    pub timestamp: Vec<u8>,   // Org timestamp
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct PureMessageInput {
+    pub query: BTreeMap<String, Vec<u8>>, // xxx -> encoded query result
+    pub command: Vec<u8>,                 // Persisted message
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PureMessageEvent {
+    pub context: DigestId,
+    pub payload: Vec<u8>, // Encoded event
+}
+
+pub type PureMsgResult = PureResult<PureEvent>;
+
+// When a client wants to execute a command
+pub fn handle_command(msg: PureCommand) {
+    // check channel
+    // check context is valid
+    // get context, queries and previous from channel db
+    // create message input
+    // execute wasm with input
+    // check result event and update channel db(events, latest message)
+    // create message and timestamp
+    // propagate message to orgs
+}
+
+// When an executed message propagated
+pub fn handle_message(msg: PureCommand) {
+    // check channel
+    // check previous is last event
+    // check timestamp is valid
+    // check context is valid
+    // get queries from channel db
+    // create message input
+    // execute wasm with input
+    // check result event and update channel db
+}
--- examples/wasmsg/core/src/lib.rs
+++ examples/wasmsg/core/src/lib.rs
@@ -3,13 +3,11 @@ use purewasm_core::DigestId;
 use purewasm_core::PureResult;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct PureMessage {
-    pub previous: DigestId,                // Previous message id
-    pub context: DigestId,                 // wasm identifier
-    pub query: BTreeMap<String, DigestId>, // db/xxx -> id
-    pub command: Vec<u8>,                  // Encoded message payload
-    pub event: DigestId,                   // id of events, should be same with wasm execution
+    pub context: DigestId,                 // Wasm identifier
+    pub query: BTreeMap<String, DigestId>, // xxx -> id
+    pub payload: Vec<u8>,                  // Encoded message payload
+    pub event: DigestId,
 }
 
 impl PureMessage {
@@ -19,16 +17,16 @@ impl PureMessage {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct PureMessageInput {
-    pub context: Vec<u8>,                 // wasm raw bytes
-    pub query: BTreeMap<String, Vec<u8>>, // db:xxx -> encoded query result
+pub struct MessageInput {
+    pub context: Vec<u8>,                 // Wasm bytes
+    pub query: BTreeMap<String, Vec<u8>>, // xxx -> encoded query result
     pub message: PureMessage,             // Persisted message
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct PureEvent {
+pub struct MessageEvent {
     pub context: DigestId,
-    pub event: Vec<u8>, // Encoded event
+    pub payload: Vec<u8>, // Encoded event
 }
 
 pub type PureMsgResult = PureResult<PureEvent>;
